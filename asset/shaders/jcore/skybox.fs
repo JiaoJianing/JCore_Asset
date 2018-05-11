3d7defaf7f49ab77ee1d860185d96c8cc5a75ab5
jiaojn <jiaojn@skong.com>
2018-05-11 17:09:45 +0800

    更新资源

---
+++ asset/shaders/jcore/skybox.fs
@@ -0,0 +1,12 @@
+#version 330 core
+out vec4 FragColor;
+
+uniform samplerCube texture_skybox;
+
+in vec3 texCoord;
+
+void main()
+{
+	FragColor = texture(texture_skybox, texCoord);
+	//FragColor = vec4(1.0);
+};
